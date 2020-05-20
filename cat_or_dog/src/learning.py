5b410114d99e8db4a89a89146e048d04fb8a18c6
enrikiko <enrikiko_91@hotmail.com>
2020-05-20 22:51:43 +0200

    Better evidence he why let from passion

--- cat_or_dog/src/learning.py
+++ cat_or_dog/src/learning.py
@@ -11,7 +11,7 @@ from flask import Flask, render_template, request, send_from_directory
 from werkzeug.utils import secure_filename
 
 # Set IA
-STEPS = 100 #8000
+STEPS = 1000 #8000
 EPOCHS = 1  #25
 VALIDATION_STEPS = 80 #2000
 

554a4478ee255fd2ab69dc8f0617b9f743d93d6b
enrikiko <enrikiko_91@hotmail.com>
2020-09-06 23:16:48 +0200

    Though house it send where

--- Artificial_Neural_Network/build.sh
+++ Artificial_Neural_Network/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 DIR=$(basename $PWD |  tr '[:upper:]' '[:lower:]')
-docker build -t --no-cache $DIR .
+docker build -t $DIR .
 docker run -it --rm --name $DIR $DIR bash
 #docker exec -it $DIR bash

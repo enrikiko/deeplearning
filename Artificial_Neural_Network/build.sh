6e996011bcd47553a4fc0bd37b226859352cde6a
enrikiko <enrikiko_91@hotmail.com>
2020-09-06 23:16:17 +0200

    Offer what school item election with scientist mrs

--- Artificial_Neural_Network/build.sh
+++ Artificial_Neural_Network/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 DIR=$(basename $PWD |  tr '[:upper:]' '[:lower:]')
-docker build -t $DIR .
+docker build -t --no-cache $DIR .
 docker run -it --rm --name $DIR $DIR bash
 #docker exec -it $DIR bash
